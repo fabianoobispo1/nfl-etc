9821f9b89d9698c21a1b91ccc6d0dd96e7cc8235
Your Name <fabianoobispo@icloud.com>
2024-12-18 07:57:42 -0300

    testes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,5 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
-/* import { Active, DataRef, Over } from '@dnd-kit/core' */
 
 /* import { ColumnDragData } from '@/components/kanban/board-column'
 import { TaskDragData } from '@/components/kanban/task-card' */
